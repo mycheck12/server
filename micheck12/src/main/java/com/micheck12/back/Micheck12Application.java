6af81c2b7c9c8836aab0c89c73ab2b8d37448ab8
김지수 <wltn2858@swu.ac.kr>
2023-10-11 15:46:11 +0900

    프로젝트 생성 및 sonarqube 설정

---
+++ micheck12/src/main/java/com/micheck12/back/Micheck12Application.java
@@ -0,0 +1,13 @@
+package com.micheck12.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Micheck12Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Micheck12Application.class, args);
+  }
+
+}
---
+++ micheck12/src/test/java/com/micheck12/back/Micheck12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.micheck12.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Micheck12ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
